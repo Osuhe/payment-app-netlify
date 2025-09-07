c2314acdfb8f8ef5d0aed4250ed54d2104c17285
Cuba Transfer Admin <admin@cuba-transfer.com>
2025-09-07 17:03:09 -0300

    Fix security issues: remove .env file and replace hardcoded PayPal credentials with serverless config

---
+++ netlify/functions/config.js
@@ -0,0 +1,28 @@
+exports.handler = async (event, context) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+    'Content-Type': 'application/json'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 200, headers, body: '' };
+  }
+
+  if (event.httpMethod === 'GET') {
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({
+        paypalClientId: process.env.PAYPAL_CLIENT_ID || ''
+      })
+    };
+  }
+
+  return {
+    statusCode: 405,
+    headers,
+    body: JSON.stringify({ error: 'Method not allowed' })
+  };
+};
